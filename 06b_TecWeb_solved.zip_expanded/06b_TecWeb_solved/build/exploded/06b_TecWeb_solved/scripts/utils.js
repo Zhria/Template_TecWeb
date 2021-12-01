8e6520156a2c964588d5728a6f4cfee3bf86be28
Zhria <traka.zakaria@gmail.com>
2021-12-01 13:59:29 +0100

    aggiustamenti

--- 06b_TecWeb_solved.zip_expanded/06b_TecWeb_solved/build/exploded/06b_TecWeb_solved/scripts/utils.js
+++ 06b_TecWeb_solved.zip_expanded/06b_TecWeb_solved/build/exploded/06b_TecWeb_solved/scripts/utils.js
@@ -1,7 +1,7 @@
 /**
  * from http://javascript.html.it/guide/leggi/95/guida-ajax/
  */
- function myGetElementById(idElemento) {
+function myGetElementById(idElemento) {
 
 	// elemento da restituire
 	var elemento;
@@ -9,7 +9,7 @@
 	// se esiste il metodo getElementById questo if sara'� 
 	// diverso da false, null o undefined
 	// e sara'� quindi considerato valido, come un true
-	if ( document.getElementById )
+	if (document.getElementById)
 		elemento = document.getElementById(idElemento);
 
 	// altrimenti e' necessario usare un vecchio sistema
@@ -25,37 +25,37 @@
 
 /**
  * from http://www.e-time.it/topics/34-ajax/8-Richiamare%20l%5C'oggetto%20XmlHttpRequest
- */ 
+ */
 function myGetXmlHttpRequest() {
 
 	// variabili 
-	var 
+	var
 		// risultato 
 		xhr = false,
 		// opzioni activeX dal pi� nuovo al pi� vecchio
-		activeXoptions = new Array( "Microsoft.XmlHttp", "MSXML4.XmlHttp", "MSXML3.XmlHttp", "MSXML2.XmlHttp", "MSXML.XmlHttp" );
+		activeXoptions = new Array("Microsoft.XmlHttp", "MSXML4.XmlHttp", "MSXML3.XmlHttp", "MSXML2.XmlHttp", "MSXML.XmlHttp");
 
 	// primo tentativo come oggetto nativo
-	try { 
-		xhr = new XMLHttpRequest(); 
-	} 
-	catch (e) { 
+	try {
+		xhr = new XMLHttpRequest();
+	}
+	catch (e) {
 		// non facciamo niente... semplicemente proviamo un altro modo
 	}
 
 	// successivi tentativi come oggetto activeX dal piu' al meno recente
-	if ( ! xhr ) {
-		var created = false; 
-		for ( var i = 0 ; i < activeXoptions.length && !created ; i++ ) {
+	if (!xhr) {
+		var created = false;
+		for (var i = 0; i < activeXoptions.length && !created; i++) {
 			try {
 				// tentativo di creazione
-				xhr = new ActiveXObject( activeXoptions[i] );
+				xhr = new ActiveXObject(activeXoptions[i]);
 				// se la creazione non fallisce il codice del blocco try continua a essere eseguito
 				created = true;
-			} 
-			catch (e) { 
+			}
+			catch (e) {
 				// non facciamo niente... semplicemente proviamo un altro modo
-			} 
+			}
 		} // for ( MSXML options )
 	} // if ( ! nativo )
 
@@ -69,7 +69,7 @@ function myGetXmlHttpRequest() {
 /*
  * from http://www.dominopower.com/issues/issue200004/howto002.html
  */
-function myGetRequestParameter ( parameterName ) {
+function myGetRequestParameter(parameterName) {
 
 	// variabili
 	// estraiamo i parametri di get dalla uri della pagina
@@ -78,27 +78,27 @@ function myGetRequestParameter ( parameterName ) {
 	// Add "=" to the parameter name (i.e. parameterName=value)
 	// torna utile nello split della query per cercare il parametro voluto
 	var parameterName = parameterName + "=";
-		
-	if ( queryString.length > 0 ) {
+
+	if (queryString.length > 0) {
 
 		// Find the beginning of the string
-		begin = queryString.indexOf ( parameterName );
+		begin = queryString.indexOf(parameterName);
 
 		// If the parameter name is not found, skip it, otherwise return the value
-		if ( begin != -1 ) {
+		if (begin != -1) {
 
 			// Add the length (integer) to the beginning
 			begin += parameterName.length;
 
 			// Multiple parameters are separated by the "&" sign
-			end = queryString.indexOf ( "&" , begin );
-	
-			if ( end == -1 ) {
+			end = queryString.indexOf("&", begin);
+
+			if (end == -1) {
 				end = queryString.length
 			}// if ( ! end )
 
 			// Return the string (unescapes special characters such as & / = etc...)
-			return unescape ( queryString.substring ( begin, end ) );
+			return unescape(queryString.substring(begin, end));
 		} // if ( begin )
 
 		// Return "null" if no parameter has been found
@@ -120,12 +120,12 @@ function myGetRequestParameter ( parameterName ) {
  * Ad esempio per collocare piu' immagini di attesa nel documento, 
  * in caso di piu' richieste AJAX contemporanee
  */
-function myGetChildByName( theElement, name ) {
-	
+function myGetChildByName(theElement, name) {
+
 	// analisi alla ricerca del nodo voluto
-	if ( theElement.childNodes )
-		for ( var i = 0 ; i < theElement.childNodes.length ; i++ )
-			if ( theElement.childNodes[i].name === name )
+	if (theElement.childNodes)
+		for (var i = 0; i < theElement.childNodes.length; i++)
+			if (theElement.childNodes[i].name === name)
 				return theElement.childNodes[i];
 
 }// myGetChildByName()
