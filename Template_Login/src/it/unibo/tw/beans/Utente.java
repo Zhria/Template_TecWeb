f887c37e4b553d5e9585230c6485d2b55ee2fdc5
Zhria <traka.zakaria@gmail.com>
2021-12-01 18:12:10 +0100

    Update Utente.java

--- Template_Login/src/it/unibo/tw/beans/Utente.java
+++ Template_Login/src/it/unibo/tw/beans/Utente.java
@@ -1,17 +1,70 @@
 package it.unibo.tw.beans;
 
+import java.time.LocalDateTime;
+
+
 public class Utente {
+	private String email;
     private String username;
     private String password;
     private boolean logged;
-
-    public Utente(String username, String password) {
-        this.username = username;
+    private int tentativi;
+    private LocalDateTime ultimaModifica;
+    private boolean isAdmin;
+    private int gruppo;
+    
+    public Utente(String email, String username, String password, int gruppo) {
+        this.email=email;
+    	this.username = username;
         this.password = password;
         this.logged=false;
+        this.isAdmin=false;
+        this.tentativi=0;
+        this.gruppo=gruppo;
+        this.ultimaModifica=LocalDateTime.now();
     }
 
-    public String getUsername() {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail (String email) {
+        this.email = email;
+    }
+    
+    public int getTentativi() {
+		return tentativi;
+	}
+
+	public void setTentativi(int tentativi) {
+		this.tentativi = tentativi;
+	}
+
+	public LocalDateTime getUltimaModifica() {
+		return ultimaModifica;
+	}
+
+	public void setUltimaModifica(LocalDateTime ultimaModifica) {
+		this.ultimaModifica = ultimaModifica;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	public int getGruppo() {
+		return gruppo;
+	}
+
+	public void setGruppo(int gruppo) {
+		this.gruppo = gruppo;
+	}
+
+	public String getUsername() {
         return username;
     }
 
