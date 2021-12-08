4143e53429eba45072da2590e0b8b18b4c5dee26
RootLeo00 <caterina.leonelli@gmail.com>
2021-12-08 14:31:45 +0100

    Update LoginServlet.java

--- Template_Login/src/servlet/LoginServlet.java
+++ Template_Login/src/servlet/LoginServlet.java
@@ -2,7 +2,6 @@ package servlet;
 
 //import com.google.gson.Gson;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
